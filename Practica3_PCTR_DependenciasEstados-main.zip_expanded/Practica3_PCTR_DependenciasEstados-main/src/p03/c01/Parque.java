8ffb3789796ba4049e4a9ecc757e5390865e173a
AitorMos <164042982+AitorMos@users.noreply.github.com>
2024-03-19 16:26:13 +0100

    Add files via upload

---
+++ Practica3_PCTR_DependenciasEstados-main.zip_expanded/Practica3_PCTR_DependenciasEstados-main/src/p03/c01/Parque.java
@@ -0,0 +1,117 @@
+package src.p03.c01;
+
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+public class Parque implements IParque{
+
+	private final int maxPersonas;
+	private int contadorPersonasTotales;
+	private Hashtable<String, Integer> contadoresPersonasPuerta;
+	
+	
+	public Parque(int personas) {
+		maxPersonas = personas;
+		contadorPersonasTotales = 0;
+		contadoresPersonasPuerta = new Hashtable<String, Integer>();
+	}
+
+	@Override
+	public synchronized void entrarAlParque(String puerta){		// TODO
+		
+		// Si no hay entradas por esa puerta, inicializamos
+		if (contadoresPersonasPuerta.get(puerta) == null){
+			contadoresPersonasPuerta.put(puerta, 0);
+		}
+		
+		// Comprobar Antes de entrar
+		comprobarAntesDeEntrar();
+		
+		// Aumentamos el contador total y el individual
+		contadorPersonasTotales++;		
+		contadoresPersonasPuerta.put(puerta, contadoresPersonasPuerta.get(puerta)+1);
+		
+		// Imprimimos el estado del parque
+		imprimirInfo(puerta, "Entrada");
+		
+		// Comprobamos invariante
+		checkInvariante();
+		
+		// Notifica cambio de estado
+		notifyAll();
+		
+	}
+	
+	@Override
+	public synchronized void salirDelParque(String puerta){
+		
+		// Si no hay entradas por esa puerta, inicializamos
+		if (contadoresPersonasPuerta.get(puerta) == null){
+			contadoresPersonasPuerta.put(puerta, 0);
+		}
+		
+		// Comprobar Antes de salir
+		comprobarAntesDeSalir();
+		
+		// Decrementamos contadores
+		contadorPersonasTotales--;		
+		contadoresPersonasPuerta.put(puerta, contadoresPersonasPuerta.get(puerta)-1);
+		
+		// Imprimimos el estado
+		imprimirInfo(puerta, "Salida");
+		
+		// Comprobamos invariante
+		checkInvariante();
+		
+		// Se notifica estado
+		notifyAll();
+		
+	}
+	
+	private void imprimirInfo (String puerta, String movimiento){
+		System.out.println(movimiento + " por puerta " + puerta);
+		System.out.println("--> Personas en el parque " + contadorPersonasTotales); //+ " tiempo medio de estancia: "  + tmedio);
+		
+		// Iteramos por todas las puertas e imprimimos sus entradas
+		for(String p: contadoresPersonasPuerta.keySet()){
+			System.out.println("----> Por puerta " + p + " " + contadoresPersonasPuerta.get(p));
+		}
+		System.out.println(" ");
+	}
+	
+	private int sumarContadoresPuerta() {
+		int sumaContadoresPuerta = 0;
+			Enumeration<Integer> iterPuertas = contadoresPersonasPuerta.elements();
+			while (iterPuertas.hasMoreElements()) {
+				sumaContadoresPuerta += iterPuertas.nextElement();
+			}
+		return sumaContadoresPuerta;
+	}
+	
+	protected void checkInvariante() {
+		assert sumarContadoresPuerta() == contadorPersonasTotales : "INV: La suma de contadores de las puertas debe ser igual al valor del contador del parte";
+		assert contadorPersonasTotales <= maxPersonas : "Aforo maximo";
+		assert contadorPersonasTotales >= 0 : "No hay mas personas";
+		
+	}
+
+	protected void comprobarAntesDeEntrar(){
+		while (contadorPersonasTotales >= maxPersonas) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	protected void comprobarAntesDeSalir(){
+		while (contadorPersonasTotales <= 0) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
---
+++ Practica3_PCTR_DependenciasEstados-main.zip_expanded/Practica3_PCTR_DependenciasEstados-main/src/p03/c01/SistemaLanzador.java
@@ -0,0 +1,22 @@
+package src.p03.c01;
+
+public class SistemaLanzador {
+	public static void main(String[] args) {
+		
+		IParque parque = new Parque(50);
+		char letra_puerta = 'A';
+		System.out.println("¡Parque abierto!");
+		for (int i = 0; i < Integer.parseInt(args[0]); i++) {
+			
+			String puerta = ""+((char) (letra_puerta++));
+			
+			// Creación de hilos de entrada
+			ActividadEntradaPuerta entradas = new ActividadEntradaPuerta(puerta, parque);
+			new Thread (entradas).start();
+			
+			// Creación de hilos de salida
+			ActividadSalidaPuerta salidas = new ActividadSalidaPuerta(puerta, parque);
+			new Thread (salidas).start();
+		}
+	}
+}
